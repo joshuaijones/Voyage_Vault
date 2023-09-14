4cf75ce3b2ce2a13a0e696989a7c328fc92b2fa5
joshuaijones <joshuairwinjones@gmail.com>
2023-09-13 22:53:19 -0600

    .

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+const gmailClicked = document.getElementById('gmailLogo')
+
+gmailClicked.addEventListener('click', gmailClickerCount());
+
+
+function gmailClickerCount() {
+    console.log('clicked Gmail')
+}
